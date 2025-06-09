634dec60a2cd4e9dab23089e0c37439a2866f7e7
Ernest <ernest.szlamczyk@swmansion.com>
2025-06-09 12:05:46 +0200

    Replaced Task scheduling with GenServer with worker pool

--- lib/quality_viewer/transcode/worker.ex
+++ lib/quality_viewer/transcode/worker.ex
@@ -1,10 +1,39 @@
 defmodule QualityViewer.Transcode.Worker do
-  def run(path, {label, resolution}, id) do
+  use GenServer
+  require Logger
+
+  def start_link(callback_pid) do
+    GenServer.start_link(__MODULE__, callback_pid)
+  end
+
+  @impl true
+  def init(callback_pid) do
+    Logger.log(
+      :info,
+      "[WORKER] started a new worker #{inspect(self())} with callback #{inspect(callback_pid)}"
+    )
+
+    {:ok, callback_pid}
+  end
+
+  @impl true
+  def handle_cast({:run, {path, quality, id}}, callback_pid) do
+    Logger.log(
+      :info,
+      "[WORKER] image transcoding scheduled in worker #{inspect(self())} for #{id} and with quality #{inspect(quality)}"
+    )
+
+    do_transcode(path, quality, id)
+    send(callback_pid, {:worker_done, self()})
+    {:noreply, callback_pid}
+  end
+
+  defp do_transcode(path, {label, resolution}, id) do
     output_path = Path.dirname(path) <> "/#{label}.mp4"
 
-    Logger.log
-
     System.cmd("ffmpeg", [
+      "-loglevel",
+      "error",
       "-i",
       path,
       "-vf",
